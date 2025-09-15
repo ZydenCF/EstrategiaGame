2fb6a881f688d546cc587656eec2a2110dd5cb2d
David Ccolcca <draigner143@gmail.com>
2025-09-15 01:41:29 -0500

    EstrategiaGame

---
+++ GameEstrategia/ICombatiente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEstrategia
+{
+    internal interface ICombatiente
+    {
+        string Nombre { get; }
+        int Vida { get; set; }
+        int Daño { get; }
+        bool EstaVivo { get; }
+        void RecibirDaño(int daño);
+    }
+}
