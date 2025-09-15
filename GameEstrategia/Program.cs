7fc675c71dc21f4f2855b9351e82414796277cd3
David Ccolcca <draigner143@gmail.com>
2025-09-15 02:45:08 -0500

    Update Program.cs

--- GameEstrategia/Program.cs
+++ GameEstrategia/Program.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace GameEstrategia
 {
-    internal class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            Juego j = new Juego(7);
+            j.Iniciar();
         }
     }
 }
