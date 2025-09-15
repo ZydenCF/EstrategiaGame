ebe6ac5051e569458decc48469cb243d1899b3f9
David Ccolcca <draigner143@gmail.com>
2025-09-15 01:59:50 -0500

    Create EstructuraRecoleccion.cs

---
+++ GameEstrategia/EstructuraRecoleccion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEstrategia
+{
+    public class EstructuraRecoleccion : Estructura
+    {
+        private int cantidadPorTurno;
+
+        public EstructuraRecoleccion(string nombre, int precio, int vida, int cantidadPorTurno, Bando bando)
+            : base(nombre, precio, vida, 0, bando)
+        {
+            this.cantidadPorTurno = cantidadPorTurno;
+        }
+
+        public override void AccionPorTurno(Juego juego, Nodo nodo)
+        {
+            if (this.EstaVivo())
+            {
+                juego.RegistrarIngresoDinero(this.cantidadPorTurno);
+                juego.AgregarHistorial(string.Format("Estructura {0} generó {1} dinero.", this.Nombre, this.cantidadPorTurno));
+            }
+        }
+    }
+}
