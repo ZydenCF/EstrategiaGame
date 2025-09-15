1355b3f97c909e971ca889d786997e8e43eaf429
David Ccolcca <draigner143@gmail.com>
2025-09-15 02:15:26 -0500

    Create Helicoptero.cs

---
+++ GameEstrategia/Helicoptero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEstrategia
+{
+    public class Helicoptero : Unidad
+    {
+        public Helicoptero(Bando bando)
+            : base("Helicoptero", 300, 120, 60, 3, bando) { }
+
+        public override bool PuedeAtacarA(Unidad objetivo)
+        {
+            if (objetivo == null) return false;
+            return (objetivo is Tanque);
+        }
+    }
+}
