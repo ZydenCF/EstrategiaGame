240a825df78e9df9c2099b2f98aa8f4004a365d3
David Ccolcca <draigner143@gmail.com>
2025-09-15 01:46:53 -0500

    Create IMovible.cs

---
+++ GameEstrategia/IMovible.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEstrategia
+{
+    public interface IMovible
+    {
+        int Velocidad { get; }
+    }
+}
